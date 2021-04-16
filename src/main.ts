48050f3124d6f9f344a62a2cb5864d78b718885b
Laishuxin <15622971239@163.com>
2021-04-16 16:56:51 +0800

    🌈style(style): move files

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import '@/assets/scss/main.scss'
+import '@/style/main.scss'
 // import 'amfe-flexible'
 import router from './router'
 import store from './store'
--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "./src/assets/scss/global.scss";`
+        additionalData: `@import "./src/style/global.scss";`
       },
     },
   },
